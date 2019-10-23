20d545ce25be528b3eb1b0544079d8610679a96f
Chenchovyi Vitalii <orgvitaliy@gmail.com>
2019-10-23 17:48:45 +0300

    test

---
+++ test/RxJS/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import {Component} from '@angular/core'
+import {interval, Subscription} from 'rxjs'
+// import {} from 'rxjs/operators'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  sub: Subscription
+
+  constructor() {
+
+    const intervalStream$ = interval(1000)
+
+    this.sub = intervalStream$.subscribe((value) => {
+      console.log(value)
+    })
+
+  }
+
+
+  stop() {
+    this.sub.unsubscribe()
+  }
+}
+
