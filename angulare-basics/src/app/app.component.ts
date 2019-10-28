473f4c9377f1f1ff0454c20f7651bb591cd56cf8
Chenchovyi Vitalii <orgvitaliy@gmail.com>
2019-10-28 10:41:41 +0200

    11 > 12

--- angulare-basics/src/app/app.component.ts
+++ angulare-basics/src/app/app.component.ts
@@ -6,34 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = `Dynamic title`
-  number = 42
-  arr = [1, 2, 3]
-  obj = {
-    a: 1,
-    b: {
-      c: 2
-    }
-  }
-  inputValue = ''
-  // img = 'https://icon-library.net/images/react-icon/react-icon-13.jpg'
 
-  constructor() {
-    // setTimeout(() => {
-    //   console.log('Timeout is over')
-    //   this.img = 'https://www.clearpeaks.com/wp-content/uploads/2017/09/Angular.png'
-    // }, 5000)
+  backgroundToggle = false
+
   }
 
-  onInput(event: KeyboardEvent) {
-    this.inputValue = (<HTMLInputElement>event.target).value;
-  }
-
-  onClick() {
-    console.log('Click')
-  }
-  onBlur(str: string) {
-    this.inputValue = str;
-  }
-
-}
--- angulare-basics/src/app/app.module.ts
+++ angulare-basics/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { Post4Component } from './post4/post4.component';
     Post4Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
