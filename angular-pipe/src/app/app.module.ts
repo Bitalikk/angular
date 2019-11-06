dee1f90e090391fc01f46eadfd18b894020a43a7
Chenchovyi Vitalii <orgvitaliy@gmail.com>
2019-11-06 18:05:43 +0200

    05-01video

--- angular-pipe/src/app/app.module.ts
+++ angular-pipe/src/app/app.module.ts
@@ -2,16 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
-import { StyleDirective } from './directives/style.directive';
-
 import { AppComponent } from './app.component';
-import { IfnotDirective } from './directives/ifnot.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StyleDirective,
-    IfnotDirective,
+    AppComponent
   ],
   imports: [
     BrowserModule,
